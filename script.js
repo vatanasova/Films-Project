4f2f092c2ea2516070d73a77e7b24942df89cf5e
Victoria Atanasova <victoria.atanasova@netreact.com>
2021-01-26 15:44:39 +0200

    Functions

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 "use strict"
 
-const moviesCnt = +prompt("Movies count", "");
+let moviesCnt;
+
+function start() {
+    do {
+        moviesCnt = +prompt("Movies count", "");
+    } while (moviesCnt == null || moviesCnt == '' || isNaN(moviesCnt))
+}
+
+start();
 
 const personalMovieDb = {
     count: moviesCnt,
@@ -10,34 +18,63 @@ const personalMovieDb = {
     private: false
 };
 
-for (let i = 0; i < moviesCnt; i ++) {
+function saveMovies() {
+    for (let i = 0; i < moviesCnt; i ++) {
 
-    let movieName = "";
-    let movieRating = "";
-
-    // do {
-    //     movieName = prompt("Movie name", "");
-    // } while (movieName == null || movieName == "" || movieName.length > 50);
-
-    while (movieName == null || movieName == "" || movieName.length > 50) {
-        movieName = prompt("Movie name", "");
-    }
+        let movieName = "";
+        let movieRating = "";
     
-    while (movieRating == null || movieRating == "") {
-        movieRating = +prompt("Movie rating", "");
-    } 
-
-    personalMovieDb.movies[movieName] = movieRating;
+        // do {
+        //     movieName = prompt("Movie name", "");
+        // } while (movieName == null || movieName == "" || movieName.length > 50);
+    
+        while (movieName == null || movieName == "" || movieName.length > 50) {
+            movieName = prompt("Movie name", "");
+        }
+        
+        while (movieRating == null || movieRating == "") {
+            movieRating = +prompt("Movie rating", "");
+        } 
+    
+        personalMovieDb.movies[movieName] = movieRating;
+    }
 }
 
-if (personalMovieDb.count < 10) {
-    alert("Too few movies watched");
-} else if (personalMovieDb.count <= 30) {
-    alert("Classical watcher");
-} else if (personalMovieDb.count > 30) {
-    alert("Movie fan");
-} else {
-    alert("Error occured");
+saveMovies();
+
+function detectPersonalLevel() {
+    if (personalMovieDb.count < 10) {
+        alert("Too few movies watched");
+    } else if (personalMovieDb.count <= 30) {
+        alert("Classical watcher");
+    } else if (personalMovieDb.count > 30) {
+        alert("Movie fan");
+    } else {
+        alert("Error occured");
+    }
 }
 
-console.log({ personalMovieDb });
+detectPersonalLevel();
+
+const showMyDB = () => { 
+    if (!personalMovieDb.private) 
+        console.log({ personalMovieDb });
+}
+
+showMyDB();
+
+const genres = [];
+
+const writeYourGenres = () => {
+
+    for (let i = 1; i <= 3; i ++) {
+        genres[i-1] = prompt(`Your favourite genre ${i}`, '');
+    }
+}
+
+writeYourGenres();
+
+console.log({ genres });
+
+
+
