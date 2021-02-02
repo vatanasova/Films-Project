1a4b5c3a2abb21beb5be98deca5374bd1a2172e9
Victoria Atanasova <victoria.atanasova@netreact.com>
2021-02-02 15:30:54 +0200

    Objects

--- script.js
+++ script.js
@@ -1,80 +1,85 @@
-"use strict"
-
-let moviesCnt;
-
-function start() {
-    do {
-        moviesCnt = +prompt("Movies count", "");
-    } while (moviesCnt == null || moviesCnt == '' || isNaN(moviesCnt))
-}
-
-start();
-
 const personalMovieDb = {
-    count: moviesCnt,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: false,
+    start: () => {
+        do {
+            this.count = +prompt("Movies count", "");
+        } while (this.count == null || this.count == '' || isNaN(this.count))
+    },
+    save: () => {
+        for (let i = 0; i < this.count; i ++) {
+
+            let movieName = "";
+            let movieRating = "";
+        
+            // do {
+            //     movieName = prompt("Movie name", "");
+            // } while (movieName == null || movieName == "" || movieName.length > 50);
+        
+            while (movieName == null || movieName == "" || movieName.length > 50) {
+                movieName = prompt("Movie name", "");
+            }
+            
+            while (movieRating == null || movieRating == "") {
+                movieRating = +prompt("Movie rating", "");
+            } 
+        
+            personalMovieDb.movies[movieName] = movieRating;
+        }
+    },
+    detectPersonalLevel: () => {
+        if (this.count < 10) {
+            alert("Too few movies watched");
+        } else if (this.count <= 30) {
+            alert("Classical watcher");
+        } else if (this.count > 30) {
+            alert("Movie fan");
+        } else {
+            alert("Error occured");
+        }
+    },
+    showMyDB: () => {
+        if (!this.private) {
+            console.log({ personalMovieDb });
+        }   
+    },
+    toggleVisibleMyDB: () => { this.private = !this.private },
+    writeGenres: () => {
+        // for (let i = 1; i <= 3; i ++) {
+
+        //     do {
+        //         genre = prompt(`Your favourite genre ${i}`, '');
+        //     } while (genre == null || genre == "");
+
+        //     personalMovieDb.genres[i-1] = genre;
+        // }
+
+        do {
+            genres = prompt(`Your favourite genres`, '');
+        } while (genres == null || genres == "");
+
+        personalMovieDb.genres = genres.split(',');
+        personalMovieDb.genres.sort();
+
+        personalMovieDb.genres.forEach((element, index) => {
+            console.log(`Favourite genre ${index+1} - ${element}`);
+        });
+    }
 };
 
-function saveMovies() {
-    for (let i = 0; i < moviesCnt; i ++) {
+personalMovieDb.start();
 
-        let movieName = "";
-        let movieRating = "";
-    
-        // do {
-        //     movieName = prompt("Movie name", "");
-        // } while (movieName == null || movieName == "" || movieName.length > 50);
-    
-        while (movieName == null || movieName == "" || movieName.length > 50) {
-            movieName = prompt("Movie name", "");
-        }
-        
-        while (movieRating == null || movieRating == "") {
-            movieRating = +prompt("Movie rating", "");
-        } 
-    
-        personalMovieDb.movies[movieName] = movieRating;
-    }
-}
+personalMovieDb.save();
 
-saveMovies();
+personalMovieDb.detectPersonalLevel();
 
-function detectPersonalLevel() {
-    if (personalMovieDb.count < 10) {
-        alert("Too few movies watched");
-    } else if (personalMovieDb.count <= 30) {
-        alert("Classical watcher");
-    } else if (personalMovieDb.count > 30) {
-        alert("Movie fan");
-    } else {
-        alert("Error occured");
-    }
-}
+personalMovieDb.showMyDB();
 
-detectPersonalLevel();
+personalMovieDb.toggleVisibleMyDB();
 
-const showMyDB = () => { 
-    if (!personalMovieDb.private) 
-        console.log({ personalMovieDb });
-}
+personalMovieDb.showMyDB();
 
-showMyDB();
-
-const genres = [];
-
-const writeYourGenres = () => {
-
-    for (let i = 1; i <= 3; i ++) {
-        genres[i-1] = prompt(`Your favourite genre ${i}`, '');
-    }
-}
-
-writeYourGenres();
-
-console.log({ genres });
-
-
-
+personalMovieDb.writeGenres();
